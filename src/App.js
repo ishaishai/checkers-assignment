5125969fda96d14358f34089fc4c1b480532028b
Ishai Yaish <36458741+ishaishai@users.noreply.github.com>
2021-09-22 00:58:22 +0300

    added mobile styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Board from './components/Board';
 import Menu from './components/Menu';
 import './components/style.css';
+import './components/mobile-style.css';
 
 function App() {
     const [start,setStart] = useState(false); 
